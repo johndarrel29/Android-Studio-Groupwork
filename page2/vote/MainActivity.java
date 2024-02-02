d015a389ff4f2fa763f7b4f75e58e866b0db29f8
Franze Young <120648811+frnzyng@users.noreply.github.com>
2024-02-02 16:26:11 +0800

    Add files via upload

---
+++ page2/vote/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.vote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vote);
+
+        AutoCompleteTextView inputPresident = findViewById(R.id.inputPresident);
+        String[] presidentCandidates = Candidate.presidentCandidates;
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, presidentCandidates);
+        inputPresident.setAdapter(adapter1);
+
+        AutoCompleteTextView inputVicePresident = findViewById(R.id.inputVicePresident);
+        String[] vicePresidentCandidates = Candidate.vicePresidentCandidates;
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, vicePresidentCandidates);
+        inputVicePresident.setAdapter(adapter2);
+
+
+        Button buttonSubmit = findViewById(R.id.buttonSubmit);
+
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String chosenPresident = inputPresident.getText().toString().trim();
+                String chosenVicePresident = inputVicePresident.getText().toString().trim();
+
+                boolean talliedVote = tallyVote(chosenPresident, chosenVicePresident);
+
+                if (talliedVote) {
+                    Intent intent = new Intent(MainActivity.this, Display.class);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    public boolean tallyVote(String presidentCandidate, String vicePresidentCandidate) {
+        String president;
+        String vicePresident;
+
+        Candidate candidatePresident1 = new Candidate();
+        Candidate candidatePresident2 = new Candidate();
+        candidatePresident1.setName("Thea Nery");
+        candidatePresident2.setName("Emlan Implica");
+
+        Candidate candidateVicePresident1 = new Candidate();
+        Candidate candidateVicePresident2 = new Candidate();
+        candidateVicePresident1.setName("Jasmine Saligan");
+        candidateVicePresident2.setName("Jiu Tejome");
+
+        // Check if fields are not blank or empty
+        if (presidentCandidate.isEmpty() || presidentCandidate.isBlank() || vicePresidentCandidate.isEmpty() || vicePresidentCandidate.isBlank()) {
+            Toast.makeText(MainActivity.this, "Fields should not be empty!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else {
+
+            // President
+            if (presidentCandidate.equalsIgnoreCase(candidatePresident1.getName())) {
+                president = presidentCandidate;
+                Candidate.getPresident().setName(candidatePresident1.getName());
+            }
+            else if (presidentCandidate.equalsIgnoreCase(candidatePresident2.getName())) {
+                president = vicePresidentCandidate;
+                Candidate.getPresident().setName(candidatePresident2.getName());
+            }
+            else {
+                Toast.makeText(MainActivity.this, "Invalid President Candidate!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            // Vice President
+            if (vicePresidentCandidate.equalsIgnoreCase(candidateVicePresident1.getName())) {
+                vicePresident = vicePresidentCandidate;
+                Candidate.getVicePresident().setName(candidateVicePresident1.getName());
+            }
+            else if (vicePresidentCandidate.equalsIgnoreCase(candidateVicePresident2.getName())) {
+                vicePresident = vicePresidentCandidate;
+                Candidate.getVicePresident().setName(candidateVicePresident2.getName());
+            }
+            else {
+                Toast.makeText(MainActivity.this, "Invalid Vice President Candidate!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }
+        return true;
+    }
+}
